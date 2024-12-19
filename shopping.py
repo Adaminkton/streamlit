ae0646c859165c4df7221c13d06ccb836e80ac33
Adaminkton <badtkea@proton.me>
2024-12-19 22:45:56 +0100

    Update shopping.py

--- shopping.py
+++ shopping.py
@@ -59,6 +59,6 @@ st.pyplot(fig)
 # Wykres 4: Liczba zakupów wg nazwy produktu
 st.write("### Liczba kupionych prodkutów według nazwy")
 counts = filtered_data["Item Purchased"].value_counts()
-st.write("counts: ", filtered_data["Item Purchased"].unique())
+st.write("counts: ", counts)
 fig, ax = plt.subplots()
 ax.pie(counts, label=filtered_data["Item Purchased"].unique(), autopct='%1.1f%%')
