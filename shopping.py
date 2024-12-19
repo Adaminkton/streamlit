bd05014e88c2d2603d39684d98fdc405593ea9a7
Adaminkton <badtkea@proton.me>
2024-12-19 22:20:14 +0100

    Update shopping.py

--- shopping.py
+++ shopping.py
@@ -22,6 +22,7 @@ name_filter = st.sidebar.multiselect("Nazwa produktów", data["Item Purchased"].
 filtered_data = data[(data["Age"] >= age_filter[0]) & 
                      (data["Age"] <= age_filter[1]) & 
                      (data["Category"].isin(category_filter))]
+                     (data["Item Purchased"].isin(name_filter))]
 
 # Wyświetlanie danych
 st.write("### Filtrowane dane", filtered_data)
@@ -54,3 +55,26 @@ filtered_data["Age"].hist(bins=20, ax=ax)
 ax.set_xlabel("Wiek")
 ax.set_ylabel("Liczba klientów")
 st.pyplot(fig)
+
+# Wykres 4: Liczba zakupów wg nazwy produktu
+fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+
+wedges, texts = ax.pie(data["Item Purchased"].unique(), wedgeprops=dict(width=0.5), startangle=-40)
+
+bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+kw = dict(arrowprops=dict(arrowstyle="-"),
+          bbox=bbox_props, zorder=0, va="center")
+
+for i, p in enumerate(wedges):
+    ang = (p.theta2 - p.theta1)/2. + p.theta1
+    y = np.sin(np.deg2rad(ang))
+    x = np.cos(np.deg2rad(ang))
+    horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+    connectionstyle = f"angle,angleA=0,angleB={ang}"
+    kw["arrowprops"].update({"connectionstyle": connectionstyle})
+    ax.annotate(data["Item Purchased"].unique()[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+                horizontalalignment=horizontalalignment, **kw)
+
+ax.set_title("Matplotlib bakery: A donut")
+
+plt.show()
