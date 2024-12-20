5a3b3ac8b1161f01bb88b5e595ea051728067260
Adaminkton <badtkea@proton.me>
2024-12-20 01:06:05 +0100

    Update shopping.py

--- shopping.py
+++ shopping.py
@@ -99,5 +99,5 @@ y = np.array([avg_male,avg_famele])
 x = np.array(["mężczyzna","kobieta"])
 bar_colors = ['tab:red', 'tab:blue']
 fig, ax = plt.subplots()
-ax.bar(x,y,color=bar_colors)
+ax.bar(x,y, labels=y, color=bar_colors)
 st.pyplot(fig)
