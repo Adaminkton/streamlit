1e8a9725860e5202fe386e77e738d879d3e196a9
Adaminkton <badtkea@proton.me>
2024-12-20 01:13:24 +0100

    Update shopping.py

--- shopping.py
+++ shopping.py
@@ -17,7 +17,7 @@ st.sidebar.title("Opcje analizy")
 # Filtry
 age_filter = st.sidebar.slider("Wiek klienta", int(data["Age"].min()), int(data["Age"].max()), (18, 70))
 price_filter = st.sidebar.slider("Cena", int(data["Purchase Amount (USD)"].min()), int(data["Purchase Amount (USD)"].max()), (0, 100))
-gender_filter = st.sidebar.multiselect("Płeć", data["Gender"].unique(), data["Gender"].unique())
+gender_filter = st.sidebar.multiselect("Płeć", data["Gender"].unique(), placeholder("wybierz płci"))
 category_filter = st.sidebar.multiselect("Kategorie produktów", data["Category"].unique(), data["Category"].unique())
 name_filter = st.sidebar.multiselect("Nazwa produktów", data["Item Purchased"].unique(), data["Item Purchased"].unique())
 
@@ -67,6 +67,8 @@ st.write("### Liczba kupionych prodkutów według nazwy")
 counts_names = filtered_data["Item Purchased"].value_counts()
 st.write(counts_names)
 fig, ax = plt.subplots()
+
+#funkcja tworząca podpisy w procentahc i liczbach
 def make_autopct(counts_names):
     def my_autopct(pct):
         total = sum(counts_names)
@@ -87,11 +89,13 @@ st.pyplot(fig)
 # Wykres 6: Ceny zakupów według płci
 st.write("### Ceny zakupów według płci")
 
+#filtrowanie według płci
 male_data = filtered_data[ (filtered_data["Gender"].str.contains("Male"))]
 female_data = filtered_data[ (filtered_data["Gender"].str.contains("Female"))]
+
+#liczenie średnich
 avg_male = sum(male_data["Purchase Amount (USD)"]) / len(male_data["Purchase Amount (USD)"])
 st.write("Średnia cena zakupów dla mężczyzn:", avg_male)
-
 avg_famele = sum(female_data["Purchase Amount (USD)"]) / len(female_data["Purchase Amount (USD)"])
 st.write("Średnia cena zakupów dla kobiet:", avg_famele)
 
